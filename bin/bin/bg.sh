bd1f8217c87e7c1f681e6423004e26d43f97b6a8
Steve Poulton <sp@wearemobedia.com>
2019-05-19 22:07:54 +0100

    Added ds wallpaper

---
+++ bin/bin/bg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+_screen() {
+    xwinwrap -ov -g $1 -- mpv --fullscreen\
+        --on-all-workspaces \
+        --no-stop-screensaver \
+        --loop-file --no-audio --no-osc --no-osd-bar -wid WID --quiet \
+        "$2" --background="$3" --video-zoom="$4" &
+}
+
+killall xwinwrap
+
+sleep 0.5
+
+for i in $( xrandr -q | grep ' connected' | grep -oP '\d+x\d+\+\d+\+\d+')
+do
+    _screen $i "$1" "${2:-#000000}" "${3:-0}"
+done
